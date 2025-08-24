7139f8f4ecec1cee03fac9e1dcb45390d4562ed6
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-08-24 18:29:29 +0300

    fix:  fix card consistency and spacing

--- Connect_Project/Laptops/views.py
+++ Connect_Project/Laptops/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Laptop
 # Create your views here.
 
 def home(request):
-    laptops = Laptop.objects.all()
+    laptop_list = Laptop.objects.all()
+    paginator = Paginator(laptop_list, 12) 
+
+    page_number = request.GET.get("page")
+    laptops = paginator.get_page(page_number)
+
     return render(request,"laptops/home.html", {'laptops': laptops})
